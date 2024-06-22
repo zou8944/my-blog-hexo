f4a955f01438ebb3f30fa415eb9be8b75cc30310
Floyd <zou894475@gmail.com>
2024-06-22 17:58:03 +0800

    ci: _post may be absent, create it

--- cmd/refresh_post.sh
+++ cmd/refresh_post.sh
@@ -24,9 +24,9 @@ cd ..
 echo "调整文章数据 ..."
 python check_and_fix_post.py cache/my-words-main/posts | grep ".md$" > cache/legal_post.txt
 
-pwd
 echo "移动文章到指定位置 ..."
-## 将 cache/leagl_post.txt 中的文章移动到 source/_posts/ 目录下
+# 将 cache/leagl_post.txt 中的文章移动到 source/_posts/ 目录下
+mkdir -p ../source/_posts/
 while IFS= read -r file; do
   # 复制文件到目标目录
   cp -rf "$file" ../source/_posts/
